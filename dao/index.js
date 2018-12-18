bc42a310d14a2eb7cd386996b9bcb4d6b1e59f4b
calivan <kogr2378@naver.com>
2018-12-19 01:43:53 +0900

    1219 db테스트

--- dao/index.js
+++ dao/index.js
@@ -1,7 +1,7 @@
 var mysql      = require('mysql');
 var db = mysql.createConnection({
-    host     : '127.0.0.1',
-    user     : 'root',
+    host     : 'calivan.ctk1nynf5quj.us-east-2.rds.amazonaws.com' , //'127.0.0.1',
+    user     : 'calivan',
     password : 'youngjun423!',
     database : 'calivan'
   });
