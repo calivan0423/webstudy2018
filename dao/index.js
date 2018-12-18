98267bd5d0790308989a35a00f52bd6dbf73aefa
calivan <kogr2378@naver.com>
2018-12-19 01:59:04 +0900

    Update index.js

--- dao/index.js
+++ dao/index.js
@@ -10,6 +10,7 @@ var db = mysql.createConnection({
   exports.login=function(id,pw,callback){
       db.query('SELECT * FROM calivan.users_table where user_id=? AND user_password =?',[id,pw],function(error,result,fields){
         //console.log(result);
+        console.log("11");
         if(result!=0){
             db.query('UPDATE users_table SET user_recent_date = NOW() WHERE user_id= ?',[id]);
         }
