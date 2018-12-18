e55d3980ecba27f051ffe429a66c06d203f9e8eb
calivan <kogr2378@naver.com>
2018-12-19 02:42:42 +0900

    1219

--- dao/index.js
+++ dao/index.js
@@ -1,5 +1,5 @@
 var mysql      = require('mysql');
-var db = mysql.createConnection({
+var dba = mysql.createConnection({
     host     : '127.0.0.1',
     port     : '3306',
     user     : 'root',
@@ -9,10 +9,10 @@ var db = mysql.createConnection({
 //modul.exports = client;
 
   exports.login=function(id,pw,callback){
-      db.query('SELECT * FROM calivan.users_table where user_id=? AND user_password =?',[id,pw],function(error,result,fields){
+      dba.query('SELECT * FROM calivan.users_table where user_id=? AND user_password =?',[id,pw],function(error,result,fields){
         //console.log(result);
         if(result!=0){
-            db.query('UPDATE users_table SET user_recent_date = NOW() WHERE user_id= ?',[id]);
+            dba.query('UPDATE users_table SET user_recent_date = NOW() WHERE user_id= ?',[id]);
         }
         callback(error,result);
       });
@@ -22,7 +22,7 @@ var db = mysql.createConnection({
 //실험중
 
   exports.idcheck=function(id,callback){
-      db.query('SELECT * FROM calivan.users_table where user_id = ?',[id],callback);
+      dba.query('SELECT * FROM calivan.users_table where user_id = ?',[id],callback);
     
   }
 
@@ -30,13 +30,13 @@ var db = mysql.createConnection({
   exports.join = function(id,pw,nick,email,callback){
      // var data= new Date().toLocaleDateString('se').replace(/\D/g, '')
       //var sign_date=new Date().toISOString().slice(0, 10).replace('T', ' ');
-      db.query('INSERT INTO calivan.users_table(user_id,user_password,user_nickname,user_email,user_sign_date,user_recent_date,user_image_path,user_withdraw,user_withdraw_date) VALUES(?,?,?,?,NOW(),NOW(),0,0,0)',[id,pw,nick,email],function(error,result,fields){
+      dba.query('INSERT INTO calivan.users_table(user_id,user_password,user_nickname,user_email,user_sign_date,user_recent_date,user_image_path,user_withdraw,user_withdraw_date) VALUES(?,?,?,?,NOW(),NOW(),0,0,0)',[id,pw,nick,email],function(error,result,fields){
           console.log(result);
            callback(error,result);
       });
   }
 
   exports.getuser = function(callback) {
-      db.query('select * from users_table', callback);
+      dba.query('select * from users_table', callback);
   }
 
