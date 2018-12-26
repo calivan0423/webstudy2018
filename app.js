cfbdce2c761ce93c80308803e87318fcd0203adb
calivan <kogr2378@naver.com>
2018-12-26 16:33:25 +0900

    1226
    session

--- app.js
+++ app.js
@@ -18,7 +18,7 @@ app.set('views engine', 'ejs');
 app.engine('html', require('ejs').renderFile);
 
 
-app.use(cookieParser());
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(session({
