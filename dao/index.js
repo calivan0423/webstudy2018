77cf8c1d7ad472ccb3b6c8bc6df9c088342660d1
calivan <kogr2378@naver.com>
2018-12-26 14:23:12 +0900

    1226
    test

--- dao/index.js
+++ dao/index.js
@@ -1,18 +1,18 @@
 var mysql      = require('mysql');
-var dba = mysql.createConnection({
-    host     : '127.0.0.1',
-    port     : '3306',
-    user     : 'root',
+var db = mysql.createConnection({
+    host     : 'calivan.ctk1nynf5quj.us-east-2.rds.amazonaws.com',
+    port     : '3300',
+    user     : 'calivan',
     password : 'youngjun423!',
     database : 'calivan'
   });
 //modul.exports = client;
 
   exports.login=function(id,pw,callback){
-      dba.query('SELECT * FROM calivan.users_table where user_id=? AND user_password =?',[id,pw],function(error,result,fields){
+      db.query('SELECT * FROM calivan.users_table where user_id=? AND user_password =?',[id,pw],function(error,result,fields){
         //console.log(result);
         if(result!=0){
-            dba.query('UPDATE users_table SET user_recent_date = NOW() WHERE user_id= ?',[id]);
+            db.query('UPDATE users_table SET user_recent_date = NOW() WHERE user_id= ?',[id]);
         }
         callback(error,result);
       });
@@ -22,7 +22,7 @@ var dba = mysql.createConnection({
 //실험중
 
   exports.idcheck=function(id,callback){
-      dba.query('SELECT * FROM calivan.users_table where user_id = ?',[id],callback);
+      db.query('SELECT * FROM calivan.users_table where user_id = ?',[id],callback);
     
   }
 
@@ -30,13 +30,13 @@ var dba = mysql.createConnection({
   exports.join = function(id,pw,nick,email,callback){
      // var data= new Date().toLocaleDateString('se').replace(/\D/g, '')
       //var sign_date=new Date().toISOString().slice(0, 10).replace('T', ' ');
-      dba.query('INSERT INTO calivan.users_table(user_id,user_password,user_nickname,user_email,user_sign_date,user_recent_date,user_image_path,user_withdraw,user_withdraw_date) VALUES(?,?,?,?,NOW(),NOW(),0,0,0)',[id,pw,nick,email],function(error,result,fields){
+      db.query('INSERT INTO calivan.users_table(user_id,user_password,user_nickname,user_email,user_sign_date,user_recent_date,user_image_path,user_withdraw,user_withdraw_date) VALUES(?,?,?,?,NOW(),NOW(),0,0,0)',[id,pw,nick,email],function(error,result,fields){
           console.log(result);
            callback(error,result);
       });
   }
 
   exports.getuser = function(callback) {
-      dba.query('select * from users_table', callback);
+      db.query('select * from users_table', callback);
   }
 
